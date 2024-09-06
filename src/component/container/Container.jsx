e8f4f674765a1b58ccfae88f30cf4854f07f37b6
Vishal <vishalmaddheshiya864@gmail.com>
2024-09-06 23:52:22 +0530

    fist commit

---
+++ src/component/container/Container.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Container() {
+  return (
+    <div className='w-full max-w-7xlmx-auto px-4'>
+    {children}
+    </div>
+    
+  )
+}
+
+export default Container
